6ca0cd1ef9dafde2f3c4fdbe96bc2332d015e033
mahendra-r-kamble <mhndk14@gmail.com>
2014-03-25 00:33:58 +0530

    script.sql

--- src/script.sql
+++ src/script.sql
@@ -1,9 +1,9 @@
 drop database if exists test;
 create database test;
+use test;
 
 drop table if exists customer;
 create table customer ( 
-id int(11) primary key auto_incrememt not null, 
-name varchar(45) not null, 
-surname varchar(45) not null 
-);
+	id int(11) primary key auto_increment, 
+	name varchar(45), 
+	surname varchar(45) );
